36e8e15b1d213d6eb5eaedbb6bbdc8121f24373f
nkr <nkgithubnarkuziev108@gmail.com>
2022-04-01 23:28:20 +0500

    Add generate block - demo

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,25 @@ use sha2::{Sha512, Digest};
 #[derive(Debug)]
 struct BaseMain 
 {
+	hash: String,
+	prev_hash: String,
 	time: String,
 	num: i64
 }
 
 impl BaseMain {
+	fn generate_block(&self) {
+		let block = BaseMain {
+			hash: "".to_string(),
+			prev_hash: "".to_string(),
+			time: Utc::now().format("%Y%m%d%H%M%S").to_string(),
+			num: 0
+		};
+
+		let txt: String = "1hello".to_string();
+		let hashed: String = block.hash_func(txt);
+	}
+
 	fn hash_func(&self, arg: String) -> String {
 		let mut hasher = Sha512::new();
 		hasher.update(arg);
@@ -30,25 +44,29 @@ impl BaseMain {
 	}
 
 	fn test(&self) {
-		match fs::create_dir("./data") {
+		match fs::create_dir("./data/base.json") {
 			Err(why) => println!("! {:?}", why.kind()),
 			Ok(_) => {},
 		}
 	}
 }
 
+fn zero_block() -> BaseMain {
+	BaseMain {
+		hash: "".to_string(),
+		prev_hash: "".to_string(),
+		time: Utc::now().format("%Y-%m-%d|%H:%M:%S").to_string(),
+		num: 0
+	}
+}
+
 
 fn main() {
-	let base = BaseMain {
-		time: Utc::now().format("%Y%m%d%H%M%S").to_string(),
-		num: 9
-	};
+	let mut block = zero_block();
 
-	let txt: String = "1hello".to_string();
-	let hashed: String = base.hash_func(txt); // mod hashing.rs
+	block.hash = block.hash_func(format!("{}-{}-{}-{}", block.hash, block.prev_hash, block.time, block.num));
 
-
-	println!("{:?}", hashed);
+	println!("{:#?}", block);
 }
 
 
